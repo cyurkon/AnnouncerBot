c2ce4731d0bc2dc7eb78dbe2395211894f9accff
Lucas <lkm027@gmail.com>
2020-07-16 18:17:38 -0400

    Added environment files.

---
+++ environment.py
@@ -0,0 +1,12 @@
+# NOTE: THIS FILE SHOULD NEVER BE COMMITTED!!!
+
+# This is the actual environment file and should be used by files that need access to keys that change per development environment.
+# This file and expected_environment.py should have the same keys. Expected_environment is used
+# as a list of keys that should exist per environment. Both files should have the same keys, however
+# this file should contain the actual keys and be imported where needed.
+
+# Set Slack bot token for slack api.
+SLACK_BOT_TOKEN = "<your_token_here>"
+
+# Slack channel CIDs
+ANNOUNCEMENTS_CID = "<slack_channel_id>"
---
+++ expected_environemt.py
@@ -0,0 +1,11 @@
+# NOTE: THIS FILE CAN AND SHOULD BE COMMITTED WHEN CHANGES ARE PRESENT.
+
+# This is a list of all environment keys that should exist. This file does not contain the actual keys.
+# It should be used to indicate a new key has been added. This and environment.py should contain the same keys
+# with environment.py containing the actual key values.
+
+# Set Slack bot token for slack api.
+SLACK_BOT_TOKEN = "<your_token_here>"
+
+# Slack channel CIDs
+ANNOUNCEMENTS_CID = "<slack_channel_id>"
